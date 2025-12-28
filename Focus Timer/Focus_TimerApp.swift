5250b3e992490187e0c40acbc60703dba1100258
Marsel Khasanov <m.khasanov@uzum.com>
2025-12-28 17:52:47 +0100

    Initial Commit

---
+++ Focus Timer/Focus_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Focus_TimerApp.swift
+//  Focus Timer
+//
+//  Created by needsomevibe on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Focus_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
